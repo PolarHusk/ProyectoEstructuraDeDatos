9a3dd1f281af87d882d240ce0cc9bc7fbd63d68e
Dylan <Dylan@MainPC>
2025-10-28 00:09:40 -0600

    Configuracion para serializacion y deserializacion

--- src/main/java/cr/ac/ufidelitas/proyectofinalestructuradedatos/ProyectoEstructuraDeDatos.java
+++ src/main/java/cr/ac/ufidelitas/proyectofinalestructuradedatos/ProyectoEstructuraDeDatos.java
@@ -1,9 +1,26 @@
-
 package cr.ac.ufidelitas.proyectofinalestructuradedatos;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import javax.swing.JOptionPane;
 
 public class ProyectoEstructuraDeDatos {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        SerializacionConfig config = SerializacionConfig.AbrirConfig();
+        if (config == null) {
+            System.out.println("El archivo config.json no existe creando configuracion nueva...");
+            String nombreTerminal = JOptionPane.showInputDialog("Ingrese el nombre de la terminal: ");
+            int totalBuses = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de buses por terminal (minimo 3): "));
+            Terminal terminal = new Terminal(nombreTerminal, totalBuses);
+            config = new SerializacionConfig(terminal);
+            config.GuardarConfig(terminal);
+            System.out.println("Archivo guardado");
+        } else {
+            System.out.println("Nombre de la terminal: " + config.getTermina1().getNombre());
+            System.out.println("Cantidad Buses: " + config.getTermina1().getBuses().length);
+        }
+
     }
 }
---
+++ src/main/java/cr/ac/ufidelitas/proyectofinalestructuradedatos/SerializacionConfig.java
@@ -0,0 +1,68 @@
+package cr.ac.ufidelitas.proyectofinalestructuradedatos;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class SerializacionConfig {
+    
+    private Terminal termina1;
+    private Usuario usuarios[];
+
+    public SerializacionConfig() {
+    }
+
+    public SerializacionConfig(Terminal termina1) {
+        this.termina1 = termina1;
+    }
+    
+    
+
+    public void GuardarConfig(Terminal terminal) {
+
+        Gson gson = new GsonBuilder().create();
+
+        try (FileWriter writer = new FileWriter("config.json")) {
+            gson.toJson(this, writer);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static SerializacionConfig AbrirConfig() {
+        File archivoCargar = new File("config.json");
+        Gson gson = new GsonBuilder().create();
+        if (archivoCargar.exists()) {
+            System.out.println(archivoCargar.exists());
+            try (FileReader reader = new FileReader(archivoCargar)) {
+                System.out.println(archivoCargar);
+                return gson.fromJson(reader, SerializacionConfig.class);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } 
+            return null;
+    
+    }
+
+    public Terminal getTermina1() {
+        return termina1;
+    }
+
+    public void setTermina1(Terminal termina1) {
+        this.termina1 = termina1;
+    }
+
+    public Usuario[] getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(Usuario[] usuarios) {
+        this.usuarios = usuarios;
+    }
+    
+    
+}
